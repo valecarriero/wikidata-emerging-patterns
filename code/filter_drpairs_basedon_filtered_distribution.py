a0f209f9ad1fd809117b39eb5e6c8ed64d171f8e
Valentina10 <valentinanita@hotmail.it>
2022-07-20 16:15:48 +0200

    consider only the main class as domain when filtering the most common drpairs

--- code/filter_drpairs_basedon_filtered_distribution.py
+++ code/filter_drpairs_basedon_filtered_distribution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 
-def return_filtered_distribution_drpairs(input_file, k, output_folder):
+def return_filtered_distribution_drpairs(clas, input_file, k, output_folder):
 
     drpairs_df = pd.read_table(input_file)
     drpairs_props = set(drpairs_df['property'].to_list())
@@ -26,13 +26,15 @@ def return_filtered_distribution_drpairs(input_file, k, output_folder):
 
     all_df = pd.concat(prop_drpairs_list)
 
-    output_file = output_folder + "/" + input_file.split("/")[-1].split(".")[0] + "_" + str(k*100).split(".")[0] + "_" + str(all_df.shape[0]) + ".tsv"
+    filtered_all_df = all_df.loc[all_df['domain'] == clas]
+
+    output_file = output_folder + "/" + input_file.split("/")[-1].split(".")[0] + "_" + str(k*100).split(".")[0] + "_" + str(filtered_all_df.shape[0]) + ".tsv"
     
 
-    all_df.reset_index(drop=True).to_csv(output_file, sep="\t", index=False)
+    filtered_all_df.reset_index(drop=True).to_csv(output_file, sep="\t", index=False)
 
     print(output_file)
-    return all_df
+    return filtered_all_df
 
         
 
@@ -40,11 +42,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # Parameters
+    parser.add_argument('--clas', type=str, required=True)
     parser.add_argument('--input_file', type=str, required=True)
     parser.add_argument('--k_value', type=str, required=True)
     parser.add_argument('--output_folder', type=str, required=True)
 
     args = parser.parse_args()
 
-    return_filtered_distribution_drpairs(args.input_file, args.k_value, args.output_folder)
+    return_filtered_distribution_drpairs(args.clas, args.input_file, args.k_value, args.output_folder)
 
